e729d105b5cd94b24af2c20b59c4614fc3bcb4aa
mahjabin2024 <mahjabin.mahjabin@opetus.tampere.fi>
2024-11-18 18:29:40 +0200

    class work

---
+++ script.js
@@ -0,0 +1,6 @@
+var info={"name":"  Tasbih","age":14,"nation":"Bangladesh"
+}
+document.getElementById("main").innerHTML=info.name
+document.getElementById("main2").innerHTML=info["age"]
+document.getElementById("main3").innerHTML=Math.random()
+
